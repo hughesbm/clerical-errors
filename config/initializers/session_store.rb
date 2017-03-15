199e91e9e423949fb025fc06dea41050ca5dbc78
Brenton Hughes <brenton.hughes@gmail.com>
2017-03-15 16:51:09 -0700

    Rename project

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_hero-roller_session'
+Rails.application.config.session_store :cookie_store, key: '_clerical_errors_session'
