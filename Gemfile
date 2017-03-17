02dd1a03ed85638ae59d749049f4bf0dcb0902c3
Brenton Hughes <brenton.hughes@gmail.com>
2017-03-16 17:34:31 -0700

    Install and setup Devise gem
    - Add basic flash messages
    - Add default mailer url
    - Add standard devise views

--- Gemfile
+++ Gemfile
@@ -41,6 +41,9 @@ gem 'bootstrap', '~> 4.0.0.alpha6'
 # Use Simple Form for forms
 gem 'simple_form'
 
+# Use Devise for user management
+gem 'devise'
+
 # Use tether for tooltips
 source 'https://rails-assets.org' do
   gem 'rails-assets-tether', '>= 1.3.3'
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def flash_class(type)
+    case type
+    when 'error' then 'alert-danger'
+    when 'notice' then 'alert-info'
+    when 'alert' then 'alert-warning'
+    when 'success' then 'alert-success'
+    else type.to_s
+    end
+  end
 end
