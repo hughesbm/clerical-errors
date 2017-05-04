2baf5d3f2cb1b7dc5612a0a1d664973e4ce7fda3
Brenton Hughes <brenton.hughes@gmail.com>
2017-05-04 15:10:53 -0700

    Add characters routes, controller actions, views

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,5 +1,31 @@
 class CharactersController < ApplicationController
+  # before_action :authenticate_user!, only: [:new, :create]
 
   def index
+    @characters = Character.all
+  end
+
+  def new
+    @character = Character.new
+  end
+
+  def create
+    @character = current_user.characters.create(character_params)
+    redirect_to characters_path
+  end
+
+  def show
+  end
+
+  private
+
+  def character_params
+    params.require(:character).permit(:name, :character_class, :race, :level)
+  end
+
+  helper_method :current_character
+
+  def current_character
+    @current_character ||= Character.find(params[:id])
   end
 end
