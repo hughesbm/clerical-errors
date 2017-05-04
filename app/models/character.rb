b88694ab0380b9d7f0caaa1e2ff460cb5962c5b7
Brenton Hughes <brenton.hughes@gmail.com>
2017-05-04 15:44:48 -0700

    Add class/level methods, validation to character model

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,5 +1,21 @@
 class Character < ApplicationRecord
   belongs_to :user
+  has_many :levels
 
   validates :name, presence: true, length: { minimum: 3, maximum: 30 }
+  validates :description, length: { maximum: 500 }
+
+  def level
+    levels.count
+  end
+
+  def class_levels
+    class_levels_hash = {}
+
+    levels.each do |level|
+      class_levels_hash[{ base: level.base_class, sub: level.sub_class }] += 1
+    end
+
+    class_levels_hash
+  end
 end
