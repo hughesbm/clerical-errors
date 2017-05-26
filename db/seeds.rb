f83cba4adb9b5b1888714cfc8701d01f90e2fe99
Brenton Hughes <brenton.hughes@gmail.com>
2017-05-26 13:17:25 -0700

    Add basic race seeding to db:seed task

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Dir.glob('db/seeds/races/*.json') do |file|
+  race_hash = JSON.parse(File.read(file))
+
+  Race.create(name: race_hash["name"], decription: race_hash["content"])
+end
